5be7a95416c00fd6052ecbd8be4c0e0530ebfaa2
Abdellah DAMRI <damriabdellah@gmail.com>
2024-07-06 18:09:38 +0100

    feat: adding @/lib

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,5 @@
-import { cleanEnv, str, port } from "envalid";
-
-export const env = cleanEnv(process.env, {
-  PORT: port(),
-  MONGO_URI: str({ default: "MONGO_URI" }),
-  NODE_ENV: str({
-    choices: ["development", "test", "production", "staging"],
-    default: "development",
-  }),
+export const responseObject = (msg: string, status = true, ...rest) => ({
+  success: status,
+  msg,
+  ...rest,
 });
